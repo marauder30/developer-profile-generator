17f24151acb8d940dc83b7fd2ec2e37181b1037d
Nick Koch <marauder30@gmail.com>
2020-01-15 19:28:12 -0700

    set up init and promptUser functions

--- Develop/index.js
+++ Develop/index.js
@@ -4,15 +4,41 @@ const util = require("util");
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
-const questions = [
+// const questions = [
   
-];
+// ];
+
+function promptUser() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "WOT is your (github user)name?"
+        },
+        {
+            type: "list",
+            name: "color",
+            message: "WOT is your favourite colour?",
+            choices: ["green", "blue", "pink", "red"]
+        }
+    ]);
+}
+
 
 function writeToFile(fileName, data) {
  
 }
 
-function init() {
+async function init() {
+
+    try {
+        const answers = await promptUser();
+
+        console.log(answers);
+    } catch(err) {
+        console.log(err);
+    }
 
 }
+
 init();
