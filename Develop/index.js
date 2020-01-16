64cf969db608ed1d4cf64a2e935d5e1623aa3a49
Nick Koch <marauder30@gmail.com>
2020-01-15 19:05:02 -0700

    initialized files and directories, added required packages

---
+++ Develop/index.js
@@ -0,0 +1,18 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+const questions = [
+  
+];
+
+function writeToFile(fileName, data) {
+ 
+}
+
+function init() {
+
+}
+init();
